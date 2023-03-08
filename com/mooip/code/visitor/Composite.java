39b750a5396ea4c82e9e34b86ce487d4a470365c
Masterofoneinchpunch <126126947+Masterofoneinchpunch@users.noreply.github.com>
2023-03-08 14:40:45 -0800

    Add files via upload

---
+++ com/mooip/code/visitor/Composite.java
@@ -0,0 +1,41 @@
+package com.mooip.code.visitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Composite (part of its own pattern) of the visitor.
+ * 
+ * @author masterofoneinchpunch
+ */
+public final class Composite extends Component {
+    private final List<Component> children = new ArrayList();
+
+    public Composite(int value, int depth) {
+        super(value, depth);
+    }
+
+    /**
+     * Accept is part of the visitor pattern.
+     * 
+     * @param visitor The visitor to be accepted.
+     */
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visitComposite(this);
+
+        for (Component child : children) {
+            child.accept(visitor);
+        }
+    }
+
+    /**
+     * Adding a child to the Composite.
+     * 
+     * @param child The child.
+     */
+    public void addChild(Component child) {
+        children.add(child);
+    }
+}
+
